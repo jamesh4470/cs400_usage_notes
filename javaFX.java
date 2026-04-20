60f78cce623f3dfc34fd5b8fa4de8184b78cc364
jamesh4470 <111305449+jamesh4470@users.noreply.github.com>
2026-04-20 13:40:15 -0500

    Update javaFX.java

--- javaFX.java
+++ javaFX.java
@@ -7,8 +7,17 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.geometry.Pos; 
+import javafx.geometry.Insets;
+import javafx.scene.input.KeyEvent;
+import javafx.event.ActionEvent;
+import javafx.application.Platform;
+import javafx.scene.input.MouseEvent;
 
 public class JavaFxApp extends Application {
     
@@ -16,13 +25,45 @@ public class JavaFxApp extends Application {
     public void start(Stage window) {
         System.out.println("app started");
 
-        Label label = new Label("my label");
-        Button button = new Button("my button");
+        Label label = new Label("Search Text: ");
+        // upon clicked, it will propagate up and print label clicked, borderpane clicked, scene clicked. 
+        label.addEventHandler(MouseEvent.MOUSE_CLICKED,
+                             (event) -> System.out.println("Label clicked"));
+        TextField searchText = new TextField();
+        searchText.addEventHandler(KeyEvent.KEY_TYPED, 
+                                   (event) -> System.out.println("Key typed: " + event.getCharacter()));
+        Button searchButton = new Button("Start Search");
+        searchButton.addEventHandler(ActionEvent.ACTION, 
+                                    (event) -> System.out.println("Search started: " + searchText.getText()));
+        Button closeButton = new Button("Close");
+        closeButton.addEventHandler(ActionEvent.ACTION,
+                                   (event) -> Platform.exit());
 
-        button.setLayoutY(25); // set y pos
+        // button.setLayoutY(25); // set y pos
 
-        Group group = new Group(label, button);
-        Scene scene = new Scene(group, 800, 600); // 800px wide, 600px high
+        // Group group = new Group(label, button);
+        BorderPane bp = new BorderPane();
+        bp.setCenter(searchText);
+        bp.setLeft(label);
+        bp.setAlignment(label, Pos.CENTER); // aligns label at center
+        bp.setMargin(label, new Insets(5, 5, 5, 5)); // top right bottom left
+        bp.setMargin(searchText, new Insets(5, 5, 5, 0); 
+        // with event.consume(), no more propagating up to scene. 
+        bp.addEventHandler(MouseEvent.MOUSE_CLICKED,
+                          (event) -> {System.out.println("BorderPane clicked"); event.consume();});
+        
+        HBox buttons = new HBox(60); // 60 pixels between elements
+        buttons.getChildren().add(searchButton);
+        buttons.getChildren().add(closeButton);
+        bp.setBottom(buttons);
+        buttons.setAlignment(Pos.CENTER); // align buttons to center
+        bp.setMargin(buttons, new Insets(5, 5, 5, 5));
+            
+        // Scene scene = new Scene(group, 800, 600); // 800px wide, 600px high
+        Scene scene = new Scene(bp);
+        scene.addEventHandler(MouseEvent.MOUSE_CLICKED,
+                          (event) -> System.out.println("Scene clicked"));
+
 
         window.setScene(scene);
         window.setTitle("java fx app");
